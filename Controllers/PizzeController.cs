7d54cd58c7fd11bb00be51442da231473d8cbbae
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-19 14:30:26 +0200

    first commit

---
+++ Controllers/PizzeController.cs
@@ -0,0 +1,64 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> pizzaList = db.Pizzas.ToList<Pizza>();
+                return View(pizzaList);
+            }
+        }
+
+        [Route("/menu")]
+        public IActionResult Details(int id)
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                Pizza current = db.Pizzas.Find(id);
+
+                if (current == null)
+                {
+                    return NotFound($"La pizza con id {id} non è stato trovato");
+                }
+                else
+                {
+                    return View(current);
+                }
+            }
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {   
+            if (!ModelState.IsValid)
+            {
+                return View("Create", pizza);
+            } else
+            {
+                PizzeriaContext db = new PizzeriaContext();
+                db.Pizzas.Add(pizza);
+                db.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Models/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using la_mia_pizzeria_static.Validators;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    [Table("pizzas")]
+    public class Pizza
+    {
+        public Pizza()
+        {
+    
+        }
+
+        [Key]
+        public int PizzaId { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(25, ErrorMessage = "Il nome non può avere più di 25 caratteri")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [FiveWordsValidationAttribute]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Url]
+        public string ImgPath { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Price { get; set; }
+    }
+}
